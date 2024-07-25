ea97dc1a71e351a4100888fc81bfe2d0c8d6cf67
Pavel-Kuznetsov-111 <gilrok@rambler.ru>
2024-07-25 22:13:37 +0300

    Homework_4

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,35 @@
 public class Main {
     public static void main(String[] args) {
 
+        int age = 19;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        }
+        if (age < 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+
+        int age2 = 17;
+        if (age2 >= 18) {
+            System.out.println("Если возраст человека равен " + age2 + ", то он совершеннолетний");
+        }
+        if (age2 < 18) {
+            System.out.println("Если возраст человека равен " + age2 + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+
+        int temperatura = 5;
+        if (temperatura <= 5) {
+            System.out.println("На улице ниже " + temperatura + " градусов, нужно надеть шапку");
+        }
+        if (temperatura >= 5) {
+            System.out.println("На улице выше " + temperatura + " градусов, можно идти без шапки");
+        }
+
+
+
 
 
     }
-
 
 
 }
